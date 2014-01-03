bc33b591c539d6d66d164f3de555e7d2ec722679
Han Xu <hgcrpd@gmail.com>
2014-01-02 23:21:53 -0800

    Create image class

--- classes.py
+++ classes.py
@@ -3,6 +3,10 @@ Classes for Galaxy Zoo
 """
 
 
+from scipy import misc
+from skimage import color
+
+
 class Submission(object):
     """
     Wrapper for a submission dataset
@@ -34,3 +38,21 @@ class Submission(object):
         See http://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/details/the-galaxy-zoo-decision-tree
         """
         pass
+
+
+class RawImage(object):
+    """
+    Used to load raw image files
+    """
+    def __init__(self, filename):
+        """
+        Given a file name, load the ndarray
+        """
+        self.data = misc.imread(filename)
+        self.gid = filename[0:6]
+
+    def crop(self):
+        pass
+
+    def grayscale(self):
+        self.data = color.rgb2gray(self.data).flatten()
--- explore.py
+++ explore.py
@@ -1,3 +1,10 @@
 """
 Scripts/scratchpad for data exploration
 """
+import os
+import numpy as np
+
+solutions_file = 'data/solutions_training.csv'
+solution = np.loadtxt(open(solutions_file, 'rb'), delimiter=',', skiprows=1)
+
+training_files = os.listdir('data/images_training')
