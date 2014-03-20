4290a6e7d996de2d5aeb7764bf5b734e1dd3bbc9
Han Xu <hgcrpd@gmail.com>
2014-03-19 21:21:55 -0700

    testing hand crafted features

--- hand_features_001.py
+++ hand_features_001.py
@@ -1,9 +1,15 @@
 """
 Model using more hand generated features instead of raw pixels
 """
+from __future__ import division
 import os
+import itertools
+import joblib
+from joblib import Parallel
+from skimage.morphology import disk
 from sklearn.base import BaseEstimator, TransformerMixin
-from classes import ImageIteratorMixin, logger, RawImage
+from sklearn.externals.joblib import delayed
+from classes import ImageIteratorMixin, logger, RawImage, train_solutions, chunks
 from constants import TRAIN_IMAGE_PATH, TEST_IMAGE_PATH
 import numpy as np
 
@@ -27,10 +33,10 @@ class ImageFeatureExtractor(ImageIteratorMixin, BaseEstimator, TransformerMixin)
     function that is provided when instantiated.
 
     """
-    def __init__(self, features, training=True, n_jobs=1, force_rerun=False, verbose=3, memmap=True, result_path=None):
-        self.features = features
+    def __init__(self, features, training=True, force_rerun=False, verbose=3, memmap=True, result_path=None):
+        self.features = list(features)
         self.training = training
-        self.n_jobs = n_jobs
+        self.n_jobs = 1  # Can't pickle the closures
         self.force_rerun = force_rerun
         self.verbose = verbose
         self.memmap = memmap
@@ -45,28 +51,66 @@ class ImageFeatureExtractor(ImageIteratorMixin, BaseEstimator, TransformerMixin)
     def fit(self, X=None, y=None):
         return self
 
-    def _transform(self, file_list):
-        filepath = TRAIN_IMAGE_PATH if self.training else TEST_IMAGE_PATH
+    def _transform(training, features, file_list):
+        filepath = TRAIN_IMAGE_PATH if training else TEST_IMAGE_PATH
         res = []
 
         for i, f in enumerate(file_list):
             if i % 5000 == 0:
                 logger.info("Processing image {} of {}".format(i, len(file_list)))
-                img = RawImage(os.path.join(filepath, f))
-                features = [func(img) for func in self.features]
-                res.append(np.hstack(features))
+            img = RawImage(os.path.join(filepath, f))
+            features = [func(img) for func in features]
+            res.append(np.hstack(features))
         return res
 
 
 def average_intensity_circle(r):
     """
+    TODO: Implement as object with __call__ method implemented, so that it can be pickled
     Generator for functions that calculate average intensity of pixels within radius r
     """
-    pass
+    mask = disk(r, np.bool)
+    def func(image):
+        offset = int((image.data.shape[0] - mask.shape[0]) / 2)
+        offset_end = offset + mask.shape[0]
+        res = image.data[offset:offset_end, offset:offset_end]
+        if len(res.shape) == 2:
+            # grayscale
+            return np.array([res[mask].mean()])
+        elif len(res.shape) == 3:
+            # color
+            return res[mask].mean(0)
+    return func
 
 
 def average_intensity_ring(r, w):
     """
     Calculate average intensity within a ring of radius r and width w
     """
-    pass
+    inner_mask = disk(r, np.bool)
+    outer_mask = disk(r + w, np.bool)
+    mask = np.copy(outer_mask)
+    mask[w:w+inner_mask.shape[0], w:w+inner_mask.shape[0]] = -inner_mask
+    def func(image):
+        offset = int((image.data.shape[0] - mask.shape[0]) / 2)
+        offset_end = offset + mask.shape[0]
+        res = image.data[offset:offset_end, offset:offset_end]
+        if len(res.shape) == 2:
+            # grayscale
+            return np.array([res[mask].mean()])
+        elif len(res.shape) == 3:
+            # color
+            return res[mask].mean(0)
+
+    return func
+
+
+def hand_features_001():
+    extractor = ImageFeatureExtractor(itertools.chain(
+        [average_intensity_circle(r) for r in range(5, 26, 5)],
+        [average_intensity_circle(50), average_intensity_circle(100)],
+        [average_intensity_ring(r, 3) for r in range(5, 26, 5)],
+        [average_intensity_ring(50, 3), average_intensity_ring(100, 3)],
+    ), force_rerun=True, n_jobs=5, verbose=3)
+
+    train_x = extractor.transform()
