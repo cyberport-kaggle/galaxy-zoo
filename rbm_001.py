309c6c9208a5ae3603751eeb75c0c5e0cd52d35a
Han Xu <hgcrpd@gmail.com>
2014-03-26 13:20:12 -0700

    rbm

--- rbm_001.py
+++ rbm_001.py
@@ -29,11 +29,14 @@ def rbm_001():
     scaler = MinMaxScaler()
     images = scaler.fit_transform(images)
 
+    # Training takes a long time, says 80 seconds per iteration, but seems like longer
+    # And this is only with 256 components
     rbm = BernoulliRBM(verbose=1)
     rbm.fit(images)
 
     train_x = rbm.transform(images)
     train_y = classes.train_solutions.data
 
+    # 0.138 CV on 50% of the dataset
     wrapper = ModelWrapper(models.Ridge.RidgeRFEstimator, {'alpha': 500, 'n_estimators': 500}, n_jobs=-1)
     wrapper.cross_validation(train_x, train_y, sample=0.5, parallel_estimator=True)
